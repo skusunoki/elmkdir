2b6465fb9aba36691075e1b72e03e880a9d05d10
skusunoki <skusunoki@gmail.com>
2024-04-06 09:02:44 +0900

    Initial commit of new project

---
+++ lib/elmkdir.ex
@@ -0,0 +1,18 @@
+defmodule Elmkdir do
+  @moduledoc """
+  Documentation for `Elmkdir`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Elmkdir.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elmkdir_test.exs
@@ -0,0 +1,8 @@
+defmodule ElmkdirTest do
+  use ExUnit.Case
+  doctest Elmkdir
+
+  test "greets the world" do
+    assert Elmkdir.hello() == :world
+  end
+end
